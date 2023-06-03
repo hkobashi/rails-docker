fb0dc8f5342372d69a9f0b5e7769ba5fe44c79c5
hkobashi <71940017+hkobashi@users.noreply.github.com>
2023-06-04 01:07:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /myapp/tmp/pids/server.pid
-apt-get install -y yarn
-apt remove cmdtest -y
-apt remove yarn -y
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update -y && apt-get install yarn -y
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
