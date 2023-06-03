fe97a425d2f23255fbfb9e988616148b1574876a
hkobashi <71940017+hkobashi@users.noreply.github.com>
2023-06-04 00:50:34 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ rm -f /myapp/tmp/pids/server.pid
 apt-get install -y yarn
 apt remove cmdtest -y
 apt remove yarn -y
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y && sudo apt-get install yarn -y
 
