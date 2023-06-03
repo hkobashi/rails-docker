e407e896c892b7634e82b8030096ec64723d4e57
hkobashi <71940017+hkobashi@users.noreply.github.com>
2023-06-04 01:00:08 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ apt-get install -y yarn
 apt remove cmdtest -y
 apt remove yarn -y
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get update -y && sudo apt-get install yarn -y
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update -y && apt-get install yarn -y
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
